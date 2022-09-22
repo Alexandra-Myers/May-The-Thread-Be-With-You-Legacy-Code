b4d05df0721907fc83e917da315ddcd9acae96b3
Alexandra-Myers <bia416cat@gmail.com>
2022-09-21 20:55:33 -0400

    aaaaa

--- src/main/java/de/zonlykroks/mttbwu/mixin/ServerMainMixin.java
+++ src/main/java/de/zonlykroks/mttbwu/mixin/ServerMainMixin.java
@@ -10,6 +10,8 @@ import com.mojang.serialization.Lifecycle;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import net.fabricmc.fabric.impl.datagen.FabricDataGenHelper;
+import net.fabricmc.fabric.impl.gametest.FabricGameTestHelper;
 import net.minecraft.Bootstrap;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.world.GeneratorTypes;
@@ -44,11 +46,18 @@ import net.minecraft.world.level.LevelProperties;
 import net.minecraft.world.level.storage.LevelStorage;
 import net.minecraft.world.level.storage.LevelSummary;
 import net.minecraft.world.updater.WorldUpdater;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.quiltmc.qsl.base.api.entrypoint.server.DedicatedServerModInitializer;
+import org.quiltmc.qsl.resource.loader.api.ResourceLoaderEvents;
+import org.quiltmc.qsl.resource.loader.impl.ModResourcePackUtil;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
 import java.io.File;
@@ -61,18 +70,17 @@ import java.util.UUID;
 import java.util.function.BooleanSupplier;
 
 @Mixin(Main.class)
-public class ServerMainMixin {
+public abstract class ServerMainMixin {
 	@Final
 	@Shadow
 	private static Logger LOGGER;
 
-	/**
-	 * @author zOnlyKroks
-	 * @reason because I can
-	 */
-	@DontObfuscate
-	@Overwrite(remap = false)
-	public static void main(String[] strings) {
+	@Shadow
+	private static void forceUpgradeWorld(LevelStorage.Session session, DataFixer dataFixer, boolean eraseCache, BooleanSupplier shouldContinue, GeneratorOptions generatorOptions) {
+	}
+
+	@Inject(method = "main", at = @At(value = "HEAD"), cancellable = true, remap = false)
+	private static void main(String[] strings, CallbackInfo ci) {
 		SharedConstants.createGameVersion();
 		OptionParser optionParser = new OptionParser();
 		OptionSpec<Void> optionSpec = optionParser.accepts("nogui");
@@ -107,6 +115,10 @@ public class ServerMainMixin {
 			Bootstrap.logMissing();
 			Util.startTimerHack();
 			Path path = Paths.get("server.properties");
+			if (FabricDataGenHelper.ENABLED) {
+				FabricDataGenHelper.run();
+				return;
+			}
 			ServerPropertiesLoader serverPropertiesLoader = new ServerPropertiesLoader(path);
 			serverPropertiesLoader.store();
 			Path path2 = Paths.get("eula.txt");
@@ -116,11 +128,15 @@ public class ServerMainMixin {
 				return;
 			}
 
-			if (!eulaReader.isEulaAgreedTo()) {
+			boolean eula = FabricGameTestHelper.ENABLED || eulaReader.isEulaAgreedTo();
+			if (!eula) {
 				LOGGER.info("You need to agree to the EULA in order to run the server. Go to eula.txt for more info.");
 				return;
 			}
 
+			for (var initializer : QuiltLoader.getEntrypointContainers(DedicatedServerModInitializer.ENTRYPOINT_KEY, DedicatedServerModInitializer.class)) {
+				initializer.getEntrypoint().onInitializeServer(initializer.getProvider());
+			}
 			File file = new File((String)optionSet.valueOf(optionSpec10));
 			Services services = Services.create(new YggdrasilAuthenticationService(Proxy.NO_PROXY), file);
 			String string = (String) Optional.ofNullable((String)optionSet.valueOf(optionSpec11)).orElse(serverPropertiesLoader.getPropertiesHandler().levelName);
@@ -145,99 +161,83 @@ public class ServerMainMixin {
 			}
 
 			ResourcePackManager resourcePackManager = new ResourcePackManager(ResourceType.SERVER_DATA, new ResourcePackProvider[]{new VanillaDataPackProvider(), new FileResourcePackProvider(session.getDirectory(WorldSavePath.DATAPACKS).toFile(), ResourcePackSource.PACK_SOURCE_WORLD)});
+			if (FabricGameTestHelper.ENABLED) {
+				FabricGameTestHelper.runHeadlessServer(session, resourcePackManager);
+				return;  // Do not progress in starting the normal dedicated server
+			}
 
-			WorldStem worldStem;
-			try {
-				DataPackSettings dataPackSettings = (DataPackSettings) Objects.requireNonNullElse(session.getDataPackSettings(), DataPackSettings.SAFE_MODE);
-				C_kjxfcecs.C_nrmvgbka c_nrmvgbka = new C_kjxfcecs.C_nrmvgbka(resourcePackManager, dataPackSettings, bl);
-				C_kjxfcecs.C_kculhjuh c_kculhjuh = new C_kjxfcecs.C_kculhjuh(c_nrmvgbka, CommandManager.RegistrationEnvironment.DEDICATED, serverPropertiesLoader.getPropertiesHandler().functionPermissionLevel);
-				worldStem = (WorldStem)Util.method_43499((executor) -> WorldStem.load(c_kculhjuh, (resourceManager, dataPackSettings1) -> {
-					DynamicRegistryManager.Writable writable = DynamicRegistryManager.builtInCopy();
-					DynamicOps<NbtElement> dynamicOps = RegistryOps.createAndLoad(NbtOps.INSTANCE, writable, resourceManager);
-					SaveProperties saveProperties = session.readLevelProperties(dynamicOps, dataPackSettings1, writable.allElementsLifecycle());
-					if (saveProperties != null) {
-						return Pair.of(saveProperties, writable.freeze());
-					} else {
-						LevelInfo levelInfo;
-						GeneratorOptions generatorOptions;
-						if (optionSet.has(optionSpec3)) {
-							levelInfo = MinecraftServer.DEMO_LEVEL_INFO;
-							generatorOptions = GeneratorTypes.createDemo(writable);
+			Thread worldThread = new Thread(() -> {
+				WorldStem worldStem;
+				try {
+					DataPackSettings dataPackSettings = (DataPackSettings)Objects.requireNonNullElse(session.getDataPackSettings(), ModResourcePackUtil.DEFAULT_SETTINGS);
+					C_kjxfcecs.C_nrmvgbka c_nrmvgbka = new C_kjxfcecs.C_nrmvgbka(resourcePackManager, dataPackSettings, bl);
+					C_kjxfcecs.C_kculhjuh c_kculhjuh = new C_kjxfcecs.C_kculhjuh(c_nrmvgbka, CommandManager.RegistrationEnvironment.DEDICATED, serverPropertiesLoader.getPropertiesHandler().functionPermissionLevel);
+					ResourceLoaderEvents.START_DATA_PACK_RELOAD.invoker().onStartDataPackReload(null, null);
+					worldStem = (WorldStem) Util.method_43499((executor) -> WorldStem.load(c_kculhjuh, (resourceManager, dataPackSettings1) -> {
+						DynamicRegistryManager.Writable writable = DynamicRegistryManager.builtInCopy();
+						DynamicOps<NbtElement> dynamicOps = RegistryOps.createAndLoad(NbtOps.INSTANCE, writable, resourceManager);
+						SaveProperties saveProperties = session.readLevelProperties(dynamicOps, dataPackSettings1, writable.allElementsLifecycle());
+						if (saveProperties != null) {
+							return Pair.of(saveProperties, writable.freeze());
 						} else {
-							ServerPropertiesHandler serverPropertiesHandler = serverPropertiesLoader.getPropertiesHandler();
-							levelInfo = new LevelInfo(serverPropertiesHandler.levelName, serverPropertiesHandler.gameMode, serverPropertiesHandler.hardcore, serverPropertiesHandler.difficulty, false, new GameRules(), dataPackSettings1);
-							generatorOptions = optionSet.has(optionSpec4) ? serverPropertiesHandler.getGeneratorOptions(writable).withBonusChest() : serverPropertiesHandler.getGeneratorOptions(writable);
+							LevelInfo levelInfo;
+							GeneratorOptions generatorOptions;
+							if (optionSet.has(optionSpec3)) {
+								levelInfo = MinecraftServer.DEMO_LEVEL_INFO;
+								generatorOptions = GeneratorTypes.createDemo(writable);
+							} else {
+								ServerPropertiesHandler serverPropertiesHandler = serverPropertiesLoader.getPropertiesHandler();
+								levelInfo = new LevelInfo(serverPropertiesHandler.levelName, serverPropertiesHandler.gameMode, serverPropertiesHandler.hardcore, serverPropertiesHandler.difficulty, false, new GameRules(), dataPackSettings1);
+								generatorOptions = optionSet.has(optionSpec4) ? serverPropertiesHandler.getGeneratorOptions(writable).withBonusChest() : serverPropertiesHandler.getGeneratorOptions(writable);
+							}
+
+							LevelProperties levelProperties = new LevelProperties(levelInfo, generatorOptions, Lifecycle.stable());
+							return Pair.of(levelProperties, writable.freeze());
 						}
+					}, Util.getMainWorkerExecutor(), executor)).get();
+					ResourceLoaderEvents.END_DATA_PACK_RELOAD.invoker().onEndDataPackReload(null, worldStem.resourceManager(), null);
+				} catch (Exception exception) {
+					ResourceLoaderEvents.END_DATA_PACK_RELOAD.invoker().onEndDataPackReload(null, null, exception);
+					LOGGER.warn("Failed to load datapacks, can't proceed with server load. You can either fix your datapacks or reset to vanilla with --safeMode", exception);
+					return;
+				}
 
-						LevelProperties levelProperties = new LevelProperties(levelInfo, generatorOptions, Lifecycle.stable());
-						return Pair.of(levelProperties, writable.freeze());
+				DynamicRegistryManager.Frozen frozen = worldStem.registryManager();
+				serverPropertiesLoader.getPropertiesHandler().getGeneratorOptions(frozen);
+				SaveProperties saveProperties = worldStem.saveProperties();
+				if (optionSet.has(optionSpec5)) {
+					forceUpgradeWorld(session, Schemas.getFixer(), optionSet.has(optionSpec6), () -> true, saveProperties.getGeneratorOptions());
+				}
+
+				session.backupLevelDataFile(frozen, saveProperties);
+				final MinecraftDedicatedServer dedicatedServer = MinecraftServer.startServer((threadx) -> {
+					MinecraftDedicatedServer minecraftDedicatedServer = new MinecraftDedicatedServer(threadx, session, resourcePackManager, worldStem, serverPropertiesLoader, Schemas.getFixer(), services, WorldGenerationProgressLogger::new);
+					minecraftDedicatedServer.setHostProfile(optionSet.has(optionSpec9) ? new GameProfile(null, optionSet.valueOf(optionSpec9)) : null);
+					minecraftDedicatedServer.setServerPort(optionSet.valueOf(optionSpec12));
+					minecraftDedicatedServer.setDemo(optionSet.has(optionSpec3));
+					minecraftDedicatedServer.setServerId(optionSet.valueOf(optionSpec13));
+					boolean bl1 = !optionSet.has(optionSpec) && !optionSet.valuesOf(optionSpec15).contains("nogui");
+					if (bl1 && !GraphicsEnvironment.isHeadless()) {
+						minecraftDedicatedServer.createGui();
 					}
-				}, Util.getMainWorkerExecutor(), executor)).get();
-			} catch (Exception var35) {
-				LOGGER.warn("Failed to load datapacks, can't proceed with server load. You can either fix your datapacks or reset to vanilla with --safeMode", var35);
-				return;
-			}
 
-			DynamicRegistryManager.Frozen frozen = worldStem.registryManager();
-			serverPropertiesLoader.getPropertiesHandler().getGeneratorOptions(frozen);
-			SaveProperties saveProperties = worldStem.saveProperties();
-			if (optionSet.has(optionSpec5)) {
-				forceUpgradeWorld(session, Schemas.getFixer(), optionSet.has(optionSpec6), () -> true, saveProperties.getGeneratorOptions());
-			}
-
-			session.backupLevelDataFile(frozen, saveProperties);
-			final MinecraftDedicatedServer dedicatedServer = MinecraftServer.startServer((threadx) -> {
-				MinecraftDedicatedServer minecraftDedicatedServer = new MinecraftDedicatedServer(threadx, session, resourcePackManager, worldStem, serverPropertiesLoader, Schemas.getFixer(), services, WorldGenerationProgressLogger::new);
-				minecraftDedicatedServer.setHostProfile(optionSet.has(optionSpec9) ? new GameProfile(null, optionSet.valueOf(optionSpec9)) : null);
-				minecraftDedicatedServer.setServerPort(optionSet.valueOf(optionSpec12));
-				minecraftDedicatedServer.setDemo(optionSet.has(optionSpec3));
-				minecraftDedicatedServer.setServerId(optionSet.valueOf(optionSpec13));
-				boolean bl1 = !optionSet.has(optionSpec) && !optionSet.valuesOf(optionSpec15).contains("nogui");
-				if (bl1 && !GraphicsEnvironment.isHeadless()) {
-					minecraftDedicatedServer.createGui();
-				}
-
-				return minecraftDedicatedServer;
+					return minecraftDedicatedServer;
+				});
+				Thread thread = new Thread("Server Shutdown Thread") {
+					public void run() {
+						dedicatedServer.stop(true);
+					}
+				};
+				thread.setUncaughtExceptionHandler(new UncaughtExceptionLogger(LOGGER));
+				Runtime.getRuntime().addShutdownHook(thread);
 			});
-			Thread thread = new Thread("Server Shutdown Thread") {
-				public void run() {
-					dedicatedServer.stop(true);
-				}
-			};
-			thread.setUncaughtExceptionHandler(new UncaughtExceptionLogger(LOGGER));
-			Runtime.getRuntime().addShutdownHook(thread);
+			worldThread.start();
 		} catch (Exception var36) {
 			LOGGER.error(LogUtils.FATAL_MARKER, "Failed to start the minecraft server", var36);
-		}
-	}
-
-	private static void forceUpgradeWorld(LevelStorage.Session session, DataFixer dataFixer, boolean eraseCache, BooleanSupplier shouldContinue, GeneratorOptions generatorOptions) {
-		LOGGER.info("Forcing world upgrade!");
-		WorldUpdater worldUpdater = new WorldUpdater(session, dataFixer, generatorOptions, eraseCache);
-		Text text = null;
-
-		while(!worldUpdater.isDone()) {
-			Text text2 = worldUpdater.getStatus();
-			if (text != text2) {
-				text = text2;
-				LOGGER.info(worldUpdater.getStatus().getString());
-			}
-
-			int i = worldUpdater.getTotalChunkCount();
-			if (i > 0) {
-				int j = worldUpdater.getUpgradedChunkCount() + worldUpdater.getSkippedChunkCount();
-				LOGGER.info("{}% completed ({} / {} chunks)...", new Object[]{MathHelper.floor((float)j / (float)i * 100.0F), j, i});
-			}
-
-			if (!shouldContinue.getAsBoolean()) {
-				worldUpdater.cancel();
-			} else {
-				try {
-					Thread.sleep(1000L);
-				} catch (InterruptedException var10) {
-				}
+			if (FabricGameTestHelper.ENABLED) {
+				System.exit(-1);
 			}
 		}
-
+		ci.cancel();
 	}
 }
